6d375261b7a3c9276b0575246682cb3dcb8df46d
DogGang <69455222+doggang@users.noreply.github.com>
2021-11-07 16:27:14 +0900

    Add files via upload

---
+++ x_y.js
@@ -0,0 +1,7 @@
+function action_coords(event) {
+    var x = event.offsetX; //offset(X,Y) : 현재 오브젝트 기준 위치
+    var y = event.offsetY; 
+   
+    var coords = "offsetX: " + x + ", offsetY: " + y + "<br/>";
+   document.getElementById("write").innerHTML = coords;
+}
