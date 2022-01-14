328d7322359e0a275b14407b243fd0fa64f92baa
DogGang <rlaehgusqp@naver.com>
2022-01-14 13:05:34 +0900

    생관 4,6층 변경, 자과대 4층 인문대쪽 복도 추가

---
+++ pop_up.js
@@ -0,0 +1,4 @@
+            var a = document.getElementById(this);
+            function popup(){
+                window.open('test.html', '_blank', 'width=800, height=600'); return false;
+            }
